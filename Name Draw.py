7565641a5e9acd72bdd5c4c90a128c18e6511d04
cwillmartin <99457144+cwillmartin@users.noreply.github.com>
2022-02-16 18:59:53 -0700

    Add files via upload
    Added program file

---
+++ Name Draw.py
@@ -0,0 +1,187 @@
+"""This program draws my name making each letter a different color"""
+
+"""first letter 'c' """
+
+from turtle import*
+color('gold','blue')
+begin_fill()
+for _ in range(1):
+    forward(40)
+    right(45)
+    forward(40)
+    right(135)    
+    forward(80)
+    left(90)
+    forward(120)
+    left(90)
+    forward(80)
+    right(135)
+    forward(40)
+    right(45)
+    forward(40)
+    right(45)
+    forward(40)
+    right(45)
+    forward(120)
+    right(45)
+    forward(40)
+end_fill()
+
+"""moving curser to start next letter"""
+
+color('gold','white')
+for _ in range(1):
+    right(45)
+    forward(40)
+    right(45)
+    forward(40)
+    left(45)
+    forward(2)
+
+"""second letter 'h' """
+
+color('gold','purple')
+begin_fill()    
+for _ in range(1):
+        right(90)
+        forward(120)
+        left(45)    
+        forward(40)
+        left(135)
+        forward(80)
+        right(90)
+        forward(40)
+        right(90)
+        forward(80)
+        left(135)
+        forward(40)
+        left(45)
+        forward(120)
+        left(45)
+        forward(40)
+        left(135)
+        forward(80)
+        right(90)
+        forward(40)
+        right(90)
+        forward(80)
+        left(135)
+        forward(40)
+end_fill()
+
+"""moving curser"""
+
+color('white','white')
+begin_fill()    
+for _ in range(1):
+    right(135)
+    forward(30)
+    right(90)
+    forward(129)
+    
+"""third letter 'u' """
+
+from turtle import*
+color('gold','orange')
+begin_fill()
+for _ in range(1):
+    right(135)
+    forward(40)
+    left(45)
+    forward(120)
+    left(45)
+    forward(40)
+    left(45)
+    forward(40)
+    left(45)
+    forward(40)
+    left(45)
+    forward(120)
+    left(45)
+    forward(40)
+    left(135)
+    forward(160)
+    right(90)
+    forward(40)
+    right(90)
+    forward(160)    
+end_fill()
+ 
+"""moving curser"""
+
+color('white','white')
+begin_fill()    
+for _ in range(1):
+    right(90)
+    forward(101)
+    
+"""fourth letter 'c' """    
+
+color('gold','brown')
+begin_fill()
+for _ in range(1):
+    forward(40)
+    right(45)
+    forward(40)
+    right(135)    
+    forward(80)
+    left(90)
+    forward(120)
+    left(90)
+    forward(80)
+    right(135)
+    forward(40)
+    right(45)
+    forward(40)
+    right(45)
+    forward(40)
+    right(45)
+    forward(120)
+    right(45)
+    forward(40)
+end_fill()    
+    
+"""moving curser"""
+
+color('white','white')
+for _ in range(1):
+    forward(4)
+    right(45)
+    forward(40)
+    right(45)
+    forward(44)
+    left(45)
+    forward(1)    
+    
+"""last letter 'k' """
+
+color('gold','maroon')
+begin_fill()    
+for _ in range(1):
+        right(90)
+        forward(120)
+        left(45)    
+        forward(40)
+        left(135)
+        forward(40)
+        right(135)
+        forward(60)
+        left(135)
+        forward(40)
+        left(45)
+        forward(60)
+        right(90)
+        forward(60)
+        left(45)
+        forward(40)
+        left(135)
+        forward(60)
+        right(135)
+        forward(60)
+        left(135)
+        forward(40)
+        left(45)
+        forward(40)
+end_fill()    
+    
+done()
